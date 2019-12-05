0c8b7d6f6f727a0deb512f523f1a1acb6da527db
Juan <j.juancamilojurado@gmail.com>
2019-12-04 19:35:22 -0500

    Creacion de pagina web

---
+++ index.php
@@ -0,0 +1,99 @@
+<!DOCTYPE html>
+<html lang="es">
+<head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>
+       Black Friday
+    </title>
+    <link rel="stylesheet" type="text/css" href="css/estilos.css">
+</head>
+<body>
+    
+    <div id="pagewrap">
+        <div id="header">
+                <div id="logo1">
+                    <img class="imagen1" src="imagenes/black_2.jpg">
+                </div>
+                <div id="logo2">
+                    <img class="imagen2" src="imagenes/black_2.jpg">
+                </div>
+            <div id="info">
+                <h1>Black Friday</h1>
+                <h2>Special offert</h2>
+            </div>
+            <ul id="button">
+                <li><a href="#">Inicio</a></li>
+                <li><a href="#">Misión</a></li>
+                <li><a href="#">Visión</a></li>
+                <li><a href="#footer">Contactenos</a></li>
+                
+            </ul>
+        </div>
+        <div id="content">
+            <h2>Contenido</h2>
+            <div id="parrafo1">
+                <h3>Parrafo 1</h3>
+                <p>son 4 lineas</p>
+                <p>son 4 lineas</p>
+                <p>son 4 lineas</p>
+                <p>son 4 lineas</p>
+            </div>
+            <div id="parrafo2">
+                <h4>Parrafo 2</h4>
+                <p>son 4 lineas</p>
+                <p>son 4 lineas</p>
+                <p>son 4 lineas</p>
+                <p>son 4 lineas</p>
+            </div>
+            <div id="parrafo3">
+                <h7>Parrafo3</h7>
+                <p>son 4 lineas</p>
+                <p>son 4 lineas</p>
+                <p>son 4 lineas</p>
+                <p>son 4 lineas</p>
+            </div>
+            <div id="parrafo4">
+                <h8>Parrafo4</h8>
+                <p>son 4 lineas</p>
+                <p>son 4 lineas</p>
+                <p>son 4 lineas</p>
+                <p>son 4 lineas</p>
+            </div>
+        </div>
+        
+        <div id="sidebar">
+                <div id="sidebar1">
+                    <h3>Sidebar 1</h3>
+                    <p>Texto 1</p>
+                    <p>Texto 2</p>
+                    <p>Texto 3</p>
+                    <p>Texto 4</p>
+                </div>
+                <div id="sidebar2">
+                     <h3>sidebar 2</h3>
+                     <p>Texto 1</p>
+                     <p>Texto 2</p>
+                     <p>Texto 3</p>
+                     <p>Texto 4</p>
+                </div>
+            </div>
+
+        <div id="footer">
+            <h2>Footer</h2>
+            <div id="contacto">
+                    <div id="img1">
+                            <img class="imagen3" src="imagenes/black-friday_3.jpg">
+                        </div>
+                    <div id="img2">
+                        <img class="imagen4" src="imagenes/black-friday_3.jpg">
+                    </div>
+                <p>Juan Camilo Gallego Jurado</p>
+                <p>Analisis y desarrollo de software</p>
+                
+            </div>
+            
+        </div>
+    </div>
+</body>
+</html>
